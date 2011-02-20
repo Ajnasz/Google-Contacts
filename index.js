57c1e5752f3909d1d51fda2b6f5cd9ad1be76cda
Lajos Koszti <ajnasz@ajnasz.hu>
2011-02-19 20:17:49 -0800

    JSlint

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
+/*jslint indent: 2 */
+/*global console: true, require: true, exports */
 var EventEmitter = require('events').EventEmitter;
 var GoogleClientLogin = require('googleclientlogin').GoogleClientLogin;
 
-var Contacts = function(conf) {
+var GoogleContacts = function (conf) {
   var contacts = this;
-	this.conf = conf || {};
+  this.conf = conf || {};
   this.googleAuth = new GoogleClientLogin(this.conf);
   this.googleAuth.on('error', function () {
-    console.log('an error occured on auth');
-    ;
+    console.error('an error occured on auth');
   });
-  this.googleAuth.login();
-  this.googleAuth.on('login',function() {
+  this.googleAuth.on('login', function () {
     contacts.loggedIn = true;
   });
-  this.client =  require('https');
+  this.client = require('https');
   this.on('error', function () {
     console.log('error occured in googlecontacts');
   });
+  this.googleAuth.login();
 };
-Contacts.prototype = new events.EventEmitter();
-Contacts.prototype.getContacts = function() {
+GoogleContacts.prototype = new EventEmitter();
+GoogleContacts.prototype.getContacts = function () {
   var contacts = this;
-  if(!contacts.loggedIn) {
-    this.googleAuth.on('login',function() {
-      console.log('on login');
+  if (!contacts.loggedIn) {
+    this.googleAuth.on('login', function () {
       contacts._getContacts();
     });
-    this.googleAuth.on('loginFailed',function() {
+    this.googleAuth.on('loginFailed', function () {
       console.log('login failed', this.conf);
     });
   } else {
     contacts._getContacts();
   }
-}
-Contacts.prototype._getContacts = function() {
-  var contacts = this;
-  var request = this.client.request(
+};
+GoogleContacts.prototype._onContactsReceived = function (response, resp) {
+  if (response.statusCode >= 200 && response.statusCode < 300) {
+    this.contacts = JSON.parse(resp);
+    this.emit('contactsReceived');
+  } else {
+    console.error('data: ', resp);
+  }
+};
+GoogleContacts.prototype._getContacts = function () {
+  var contacts = this, request, onEnd;
+
+  request = this.client.request(
     {
       host: 'www.google.com',
       port: 443,
       path: '/m8/feeds/contacts/default/full?max-results=3000&alt=json',
       method: 'GET',
       headers: {
-        'Authorization': 'GoogleLogin auth=' + this.googleAuth.getAuthId()
+        'Authorization': 'GoogleLogin auth=' + this.googleAuth.getAuthId(),
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
     },
-    function(response) {
+    function (response) {
       var resp = '';
 
-      response.on('data', function(data) {
+      response.on('data', function (data) {
         resp += data;
       });
 
       response.on('error', function (e) {
-        console.log('an error occured getting contacts', e);
+        console.error('an error occured getting contacts', e);
       });
 
-      response.on('end', function() {
-        console.log('erpons end');
-        if(response.statusCode >= 200 && response.statusCode < 300) {
-          contacts.contacts = JSON.parse(resp);
-          contacts.emit('contactsReceived');
-        } else {
-          console.log('data: ' + resp);
-        }
-      });
-  });
+      response.on('end', this._onContactsReceived.bind(this));
+    }
+  );
   request.end();
 };
-exports.Contacts = Contacts;
+exports.GoogleContacts = GoogleContacts;
