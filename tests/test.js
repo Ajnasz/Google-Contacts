6af6bd7b579e8fdf631ba8f2095b1082612fbe1b
Lajos Koszti <ajnasz@ajnasz.hu>
2011-07-10 12:46:59 +0200

    Throw error if some test were not run

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,7 @@ var IniReader = require('inireader').IniReader;
 var iniReader = new IniReader();
 var GoogleContacts = require('googlecontacts').GoogleContacts;
 var assert = require('assert');
+var concatsTested = false, groupsTested = false;
 iniReader.on('fileParse', function () {
   var cfg = this.param('account'), c;
   c = new GoogleContacts({
@@ -15,14 +16,22 @@ iniReader.on('fileParse', function () {
   });
   c.on('contactsReceived', function (contacts) {
     assert.ok(typeof contacts === 'object', 'Contacts is not an object');
-    console.log('contacts tested');
+    concatsTested = true;
   });
   c.on('contactGroupsReceived', function (contactGroups) {
     assert.ok(typeof contactGroups === 'object', 'Contact groups is not an object');
-    console.log('groups tested');
+    groupsTested = true;
   });
   c.getContacts();
   c.getContactGroups();
 });
 iniReader.load('/home/ajnasz/.google.ini');
+process.on('exit', function () {
+  if (!concatsTested) {
+    throw new Error('contact test failed');
+  }
 
+  if (!groupsTested) {
+    throw new Error('group test failed');
+  }
+});
