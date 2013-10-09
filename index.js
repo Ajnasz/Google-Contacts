9f031970d320ff90d75b256a209fb313bbf671f4
Lajos Koszti <ajnasz@ajnasz.hu>
2013-10-09 20:59:42 +0200

    Make possible to get a contact

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ var url = require('url');
 
 var contactsUrl = '/m8/feeds/contacts',
 	contactGroupsUrl = '/m8/feeds/groups',
+	contactUrl = '/m8/feeds/contacts',
 	typeContacts = 'contacts',
+	typeContact = 'contact',
 	typeGroups = 'groups',
 	projectionThin = 'thin';
 
-var GoogleContacts = function (conf) {
-	var contacts = this;
+function GoogleContacts(conf) {
+	"use strict";
+
 	this.conf = conf || {};
 	this.conf.service = 'contacts';
-};
+}
 
 GoogleContacts.prototype = {};
+
 util.inherits(GoogleContacts, EventEmitter);
 
 GoogleContacts.prototype.auth = function (cb) {
+	"use strict";
+
 	if (!this.googleAuth) {
 		this.googleAuth = new GoogleClientLogin(this.conf);
 		this.googleAuth.on('login', function () {
@@ -39,25 +45,55 @@ GoogleContacts.prototype.auth = function (cb) {
 		cb();
 	}
 };
+
+GoogleContacts.prototype.getContact = function (params) {
+	"use strict";
+
+	this.auth(function () {
+		this.requestContact(params);
+	}.bind(this));
+};
+
 GoogleContacts.prototype.getContacts = function (params) {
+	"use strict";
+
 	this.auth(function () {
 		this.requestContacts(params);
 	}.bind(this));
 };
+
 GoogleContacts.prototype.getContactGroups = function (params) {
+	"use strict";
+
 	this.auth(function () {
 		this.requestContactGroups(params);
 	}.bind(this));
 };
+
 GoogleContacts.prototype.onContactsReceived = function (response, data) {
+	"use strict";
+
 	this.contacts = JSON.parse(data);
 	this.emit('contactsReceived', this.contacts);
 };
+
+GoogleContacts.prototype.onContactReceived = function (response, data) {
+	"use strict";
+
+	this.contact = JSON.parse(data);
+	this.emit('contactReceived', this.contact);
+};
+
 GoogleContacts.prototype.onContactGroupsReceived = function (response, data) {
+	"use strict";
+
 	this.contactGroups = JSON.parse(data);
 	this.emit('contactGroupsReceived', this.contactGroups);
 };
-GoogleContacts.prototype.onResponse = function (request, response) {
+
+GoogleContacts.prototype.onResponse = function (type, response) {
+	"use strict";
+
 	var data = '', finished = false, onFinish;
 	// Thats a hack, because the end event is not emitted, but close yes.
 	// https://github.com/joyent/node/issues/728
@@ -65,10 +101,12 @@ GoogleContacts.prototype.onResponse = function (request, response) {
 		if (!finished) {
 			finished = true;
 			if (response.statusCode >= 200 && response.statusCode < 300) {
-				if (request.path.indexOf(contactsUrl) === 0) {
+				if (type === typeContacts) {
 					this.onContactsReceived(response, data);
-				} else if (request.path.indexOf(contactGroupsUrl) === 0) {
+				} else if (type === typeGroups) {
 					this.onContactGroupsReceived(response, data);
+				} else if (type === typeContact) {
+					this.onContactReceived(response, data);
 				}
 			} else {
 				var error = new Error('Bad response status: ' + response.statusCode);
@@ -87,8 +125,11 @@ GoogleContacts.prototype.onResponse = function (request, response) {
 	response.on('close', onFinish);
 	response.on('end', onFinish);
 };
+
 GoogleContacts.prototype.buildPath = function (type, params) {
-	var path, request, projection, pathItems;
+	"use strict";
+
+	var path, request, projection, pathItems, gUrl;
 	params = params || {};
 	pathItems = [];
 	params.alt = 'json';
@@ -99,9 +140,22 @@ GoogleContacts.prototype.buildPath = function (type, params) {
 		delete params.projection;
 	}
 
-	pathItems.push(type === typeGroups ? contactGroupsUrl : contactsUrl);
-	pathItems.push(this.conf.email);
+	if (type === typeGroups) {
+		gUrl = contactGroupsUrl;
+	} else if (type === typeContacts) {
+		gUrl = contactsUrl;
+	} else if (type === typeContact) {
+		gUrl = contactUrl;
+	}
+
+	pathItems.push(gUrl);
+	pathItems.push('default');
 	pathItems.push(projection);
+
+	if (type === typeContact) {
+		pathItems.push(this.conf.contactId);
+	}
+
 	path = url.format({
 		pathname: url.resolve('/', pathItems.join('/')),
 		search: '?' + querystring.stringify(params)
@@ -110,26 +164,45 @@ GoogleContacts.prototype.buildPath = function (type, params) {
 	// path += this.conf.email + '/' + projection + '?' + querystring.stringify(params);
 	return path;
 };
+
 GoogleContacts.prototype.get = function (type, params) {
-	var request = require('https').request({
+	"use strict";
+
+	var request, path;
+
+	path = this.buildPath(type, params);
+
+	request = require('https').request({
 		host: 'www.google.com',
 		port: 443,
-		path: this.buildPath(type, params),
+		path: path,
 		method: 'GET',
 		headers: {
-			'Authorization': 'GoogleLogin auth=' + this.googleAuth.getAuthId()
+			'Authorization': 'GoogleLogin auth=' + this.googleAuth.getAuthId(),
+			'GData-Version': '3.0'
 		}
 	}, function (response) {
-		this.onResponse(request, response);
+		this.onResponse(type, response);
 	}.bind(this)).on('error', function (e) {
 		// console.log('error getting stuff', e);
 	});
 	request.end();
 };
+
 GoogleContacts.prototype.requestContacts = function (params) {
+	"use strict";
+
 	this.get(typeContacts, params);
 };
+
 GoogleContacts.prototype.requestContactGroups = function (params) {
+	"use strict";
+
 	this.get(typeGroups, params);
 };
-exports.GoogleContacts = GoogleContacts;
+
+GoogleContacts.prototype.requestContact = function (params) {
+	"use strict";
+
+	this.get(typeContact, params);
+};
