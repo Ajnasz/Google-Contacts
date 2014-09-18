a717c4e4e8407647c7fbf6514f797b7a5b5a4f2d
long <forallandiyou@gmail.com>
2014-09-18 13:55:27 +0800

    migrate to Meteor 0.9.0

--- package.js
+++ package.js
@@ -1,11 +1,18 @@
 Package.describe({
-  summary: "Google-Contacts package"
+  summary: "Google-Contacts package",
+  version: "0.2.0",
+  git: "https://github.com/iyou/Meteor-Google-Contacts.git",
+  name: "long:google-contacts"
 });
 
 Package.on_use(function (api) {
   api.add_files(['index.js'], 'server');
   if(api.export)
     api.export('GoogleContacts');
+
+  if(api.versionsFrom){
+    api.versionsFrom('METEOR@0.9.0');
+  }
 });
 
 Npm.depends({
